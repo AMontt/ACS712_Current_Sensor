d4790cddac580d445164dd32cb77ce7074a1c304
AMontt <41563126+AMontt@users.noreply.github.com>
2020-06-13 18:23:01 -0400

    Step 0.0.1

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,16 @@
 #include <Arduino.h>
+//MODELS TO CONSIDER:
+// ----MODEL---------|---Range[A]---|--Sensibility--|
+//ACS712ELCTR-05B-T  | -5 to 5 A    | 185 mV/A      |
+//ACS712ELCTR-20A-T  | -20 to 20 A  | 100 mV/A      |
+//ACS712ELCTR-30A-T  | -30 to 30 A  | 66 mV/A       |
+//---------------------------------------------------
 
+//This sensor gives 2.5 V for a current of 0 [A], so
+//we first define a voltage called Vx = Vm-2.5
+float Vx;
+float Vm;
+//where Vm = voltage measured by the sensor
 void setup() {
   // put your setup code here, to run once:
 }
